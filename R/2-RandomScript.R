6380b1d8ab22eb0cce6d77844b1e7601d489c33d
Kenneth Feng <kennethzfeng@gmail.com>
2014-03-10 23:11:53 -0400

    3 Training Models are added.

--- R/2-RandomScript.R
+++ R/2-RandomScript.R
@@ -8,31 +8,31 @@ head(train, 5)
 df <- train[, c("Ticket", "Cabin", "Fare")]
 
 # First Letter of Cabin
-df$FirstCabin <- substr(as.character(df$Cabin), 0, 1)
+train$CabinClass <- substr(as.character(train$Cabin), 0, 1)
 
-splitted.cabin <- strsplit(as.character(df$Cabin), " ")
-df$CabinCount <- sapply(splitted.cabin, length)
+splitted.cabin <- strsplit(as.character(train$Cabin), " ")
+train$CabinCount <- sapply(splitted.cabin, length)
 
 # Divide Fare / # of Cabin
 
-df$DividedFare <- df$Fare
+train$IndividualFare <- train$Fare
 
-boolean <- df$CabinCount != 0
-df$DividedFare[boolean] <- df$Fare[boolean] / df$CabinCount[boolean]
+boolean <- train$CabinCount != 0
+train$IndividualFare[boolean] <- train$Fare[boolean] / train$CabinCount[boolean]
 
-df$FirstCabin <- as.numeric(df$FirstCabin)
-df$DividedFare <- as.numeric(df$DividedFare)
+df$CabinClass <- as.numeric(df$CabinClass)
+df$IndividualFare <- as.numeric(df$IndividualFare)
 
 temp <- df[boolean, ]
 temp
-boxplot(temp$DividedFare ~ temp$FirstCabin)
+boxplot(temp$IndividualFare ~ temp$CabinClass)
 
 # Decision Tree
 
 
 
 # Split Ticket by space
-splitted.ticket <- strsplit(as.character(df$Ticket), " ")
+splitted.ticket <- strsplit(as.character(train$Ticket), " ")
 
 join.front <- function(x) {
   return.value <- NA
@@ -45,18 +45,100 @@ join.front <- function(x) {
 #install.packages("gdata")
 library(gdata)
 
-df$Front <- trim(sapply(splitted.ticket, join.front))
+train$Front <- trim(sapply(splitted.ticket, join.front))
 table(sapply(splitted.ticket, length))
-df$Last <- trim(sapply(splitted.ticket,
+train$Last <- trim(sapply(splitted.ticket,
                   function(x) {x[length(x)]}))
 
+# split the train by 70%:30%
+set.seed(-1025)
+train.sample <- train[sample(nrow(train), nrow(train) * 0.7), ]
+indexes <- as.vector(train.sample[, 1])
+test.sample <- subset(train, !train[, 1] %in% indexes)
+
+# Building Model
+
+names(train.sample)
+
+# Logistic Model
+interesting.columns <- c("Pclass", "Sex", "age2", "Parch", "SibSp", "IndividualFare", "CabinClass", "Embarked", "Survived")
+
+interesting.columns <- c("Pclass", "Sex", "age2", "Parch", "SibSp", "IndividualFare", "Embarked", "Survived")
+df <- train.sample[, interesting.columns]
 
 
-sum(is.na(as.numeric(df$Last)))
-length(df$Last)
-sum(!is.na(as.numeric(df$Last)))
+model.log <- glm(Survived ~ ., data=df, family="binomial")
 
-df$nChar <- sapply(df$Last, nchar)
+predicted <- predict(model.log, type="response")
 
-table(df$nChar)
-df[df$nChar == 5, ]
+train.prediction <- rep(0, nrow(df))
+train.prediction[predicted > 0.5] = 1
+table(train.prediction, df$Survived)
+mean(train.prediction == df$Survived)
+
+test.predicted <- predict(model.log, test.sample, type="response")
+test.prediction <- rep(0, nrow(test.sample))
+test.prediction[test.predicted > 0.5] = 1
+model.log.table <- table(test.prediction, test.sample$Survived)
+model.log.table
+model.log.mean <- mean(test.prediction == test.sample$Survived)
+model.log.mean
+
+summary(model.log)
+
+# Model tree
+#install.packages("MASS")
+library(tree)
+
+sapply(df, class)
+df.new <- na.omit(df)
+
+model.tree <- tree(Survived ~ ., data=df)
+
+predicted <- predict(model.tree, df)
+
+train.prediction <- rep(0, nrow(df))
+train.prediction[predicted > 0.5] = 1
+table(train.prediction, df$Survived)
+mean(train.prediction == df$Survived)
+
+test.predicted <- predict(model.tree, test.sample)
+test.prediction <- rep(0, nrow(test.sample))
+test.prediction[test.predicted > 0.5] = 1
+table(test.prediction, test.sample$Survived)
+mean(test.prediction == test.sample$Survived)
+
+
+# Model QDA
+#install.packages("MASS")
+library(MASS)
+
+sapply(df, class)
+df.new <- na.omit(df)
+
+names(df)
+sapply(df, class)
+model.qda <- qda(Survived ~ as.factor(Sex) + age2 + as.factor(Pclass)
+                 + Parch + SibSp + IndividualFare, data=df)
+
+predicted <- predict(model.qda, df)$class
+summary(predicted)
+table(predicted, df$Survived)
+mean(predicted == df$Survived)
+
+test.predicted <- predict(model.qda, test.sample)$class
+table(test.predicted, test.sample$Survived)
+model.qda.mean <- mean(test.predicted == test.sample$Survived)
+model.qda.mean
+
+
+#sum(is.na(as.numeric(df$Last)))
+#length(df$Last)
+#sum(!is.na(as.numeric(df$Last)))
+
+#df$nChar <- sapply(df$Last, nchar)
+
+#table(df$nChar)
+#df[df$nChar == 5, ]
+
+
